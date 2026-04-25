99a9a9570df20da67523720f1ea410a809136128
nguyenthimyhan-26 <myhann260@gmail.com>
2026-04-25 09:59:56 +0700

    Add files via upload

---
+++ src/main/java/hce_java/hce_java_sat_spring/HceJavaSatSpringApplication.java
@@ -0,0 +1,13 @@
+package hce_java.hce_java_sat_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HceJavaSatSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HceJavaSatSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hce_java/hce_java_sat_spring/HceJavaSatSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package hce_java.hce_java_sat_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HceJavaSatSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
